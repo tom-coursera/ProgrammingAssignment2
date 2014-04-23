ab9b83519c683a407ea95b4087b76fac50e1e161
unknown <tttr@cnu1071s8y.bkkhq.chevrontexaco.net>
2014-04-23 19:24:07 +0700

    commit my first edit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2 -- Prof. Roger Peng -- Coursera
+#
+## I basically modify the "cachevector.R" template 
+## the mean calculation in "cachevector" was replaced with "solve" 
+## which calculates the matrix inverse
+## no error checking -- assume input matrix is a square invertible matrix
 
-## Write a short comment describing this function
+## this function creates a cached matrix
+## to test this function, type, for example, the following R commands and you should see
+## a randomly generated 6x6 square matrix
+
+## a_matrix <- matrix(rnorm(36, 0, 1), nrow = 6, ncol = 6)
+## a_cache_matrix <- makeCacheMatrix(a_matrix)
+## a_cache_matrix$get()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL 
+  }
+  get <- function() x
+  set_inverse <- function(inv) inverse <<- inv
+  get_inverse <- function() inverse
+  list(set = set, get = get, set_inverse = set_inverse, get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
+## this function calculates the cached inverse of a square matrix
+## to test this function, type the following
+
+## cacheSolve(a_cache_matrix)
+
+## to test that the inverse matrix is correct, type the following
+## cacheSolve(a_cache_matrix) %*% a_cache_matrix$get()
+## you should now see a 6x6 identity matrix printed in the R console
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$get_inverse()
+  if (!is.null(inverse)) {
+    message("getting cached inverse")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data)
+  x$set_inverse(inverse)
+  inverse
 }
